e3514314032e1df880aa6bafdddf3ba0dcdcf034
Margherita Balzoni - margherita.balzoni@studio.unibo.it <margherita.balzoni@studio.unibo.it>
2022-11-28 13:02:49 +0100

    implementato metodo 3

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -111,6 +111,8 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
+        final Map<K, V> m = new HashMap<>();
+        map.forEach((k,v) -> m.put(k, v.orElse(def.get())));
         return null;
     }
 
